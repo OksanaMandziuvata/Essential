7359f176ae51b6fec85ac8da58a0433e7b23e5ca
Oksana Mandziuvata <okm99459@gmail.com>
2025-07-17 21:40:46 +0300

    lesson 4

---
+++ lesson 4/4 task 2/AbstractHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_2
+{
+    abstract class AbstractHandler
+    {
+        protected string fileName;
+
+        public AbstractHandler(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void Open()
+        {
+            Console.WriteLine(fileName + " opened");
+        }
+
+        public void Create()
+        {
+            Console.WriteLine(fileName + " created");
+        }
+
+        public void Change()
+        {
+            Console.WriteLine(fileName + " modified");
+        }
+
+        public abstract void Save();
+    }
+}
---
+++ lesson 4/4 task 2/XMLHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_2
+{
+    class XMLHandler : AbstractHandler
+    {
+        public XMLHandler(string fileName)
+            : base(fileName)
+        {
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine(fileName + " saved as XML format");
+        }
+    }
+}
